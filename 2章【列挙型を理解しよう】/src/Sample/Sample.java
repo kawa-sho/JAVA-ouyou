9891ef496210c2ee3d3d3affa29fede721871eb0
kawa-sho <101601548+kawa-sho@users.noreply.github.com>
2022-08-14 15:57:29 +0900

    Finish 2章

---
+++ 2章【列挙型を理解しよう】/src/Sample/Sample.java
@@ -0,0 +1,9 @@
+package Sample;
+
+public class Sample {
+	public static void main(String[] args) {
+		Dessert.APPLE.advertise(); // 「まっかなりんごが100円！」と出力
+		Dessert.ICE_CREAM.advertise(); // 「夏にぴったりアイスクリームが200円！」と出力
+		Dessert.CAKE.advertise(); // 「皆大好き甘いケーキが300円！」と出力
+	}
+}
